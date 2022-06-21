653e1f967c2759e1f0ecd8d066871890365cef83
songa1 <typa2611@gmail.com>
2022-06-21 11:15:21 +0200

    create deployment github action workflow

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ApolloServer } from "apollo-server";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 import { schema } from "./schema";
 import { context } from "./context"; 
@@ -8,11 +8,11 @@ import { context } from "./context";
 export const server = new ApolloServer({
     schema,
     context,
-    introspection:true,
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+    introspection: true,                                      
+    plugins: [ApolloServerPluginLandingPageLocalDefault()], 
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen({port}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
